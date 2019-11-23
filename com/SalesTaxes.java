1ddb9ab43d867d668ea061008893d091440fcb46
daniel <g.thirteen333@gmail.com>
2019-11-23 17:18:05 -0500

    import check function added

--- com/SalesTaxes.java
+++ com/SalesTaxes.java
@@ -9,14 +9,11 @@ import java.util.Arrays;
 
 public class SalesTaxes {
 
-    public static boolean isTrue(Map<Character, Map<Character, Integer>> graph, Character char1, Character char2){
-        
-        if (graph.containsKey(char1) && graph.get(char1).containsKey(char2)){
-            return true;
-        } else {
-            return false;
-        }
+    public static boolean isImported(String list){  // Check to see if it's imported      
+       return list.indexOf("import") != -1 ? true : false;
     }
+
+    
 
     public static void main(String[] args) throws Exception{ //Main function
         // Read data file
@@ -26,9 +23,25 @@ public class SalesTaxes {
         ArrayList<String> arlist = new ArrayList<String>();
 
         while ((st = br.readLine()) != null) arlist.add(st); // Read first line
-      
 
-        System.out.println(arlist);
+
+        int count = 0;
+        double price = 0.0;
+        boolean imported = isImported(arlist.get(1));
+
+
+
+
+
+
+        double num = 4.1985;
+        double rounded = Math.ceil(7.63 * 20) / 20.0; // round up to nearest 0.05
+        
+        System.out.println(rounded);
+        System.out.println(arlist.get(3)); // get element from array list
+        System.out.println(arlist.get(3).indexOf("chocolate") != -1 ? true : false ); // check to see if a string has a word  
+        // this even works if chocolate is plural
+        System.out.println(imported);
     }
     
 } 
