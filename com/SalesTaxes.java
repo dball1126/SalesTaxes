8fce9165f65c0a3abead5821cb8f7e46c32df641
daniel <g.thirteen333@gmail.com>
2019-11-23 17:34:30 -0500

    tax type function complete

--- com/SalesTaxes.java
+++ com/SalesTaxes.java
@@ -13,7 +13,17 @@ public class SalesTaxes {
        return list.indexOf("import") != -1 ? true : false;
     }
 
-    
+    public static String taxType(String list){
+        
+        String type = "";
+        if (list.indexOf("headache") != -1 || list.indexOf("pill") != -1 || list.indexOf("chocolate") != -1 ||
+           list.indexOf("book") != -1 || list.indexOf("food") != -1){
+               type = "No basic sales tax";
+        } else {
+            type = "Yes basic sales tax";
+        }
+        return type;
+    }
 
     public static void main(String[] args) throws Exception{ //Main function
         // Read data file
@@ -28,22 +38,18 @@ public class SalesTaxes {
         int count = 0;
         double price = 0.0;
         boolean imported = isImported(arlist.get(1));
-
-
-
-
-
+        String taxType = taxType(arlist.get(2));
 
         double num = 4.1985;
         double rounded = Math.ceil(7.63 * 20) / 20.0; // round up to nearest 0.05
         
         System.out.println(rounded);
         System.out.println(arlist.get(3)); // get element from array list
-        System.out.println(arlist.get(3).indexOf("chocolate") != -1 ? true : false ); // check to see if a string has a word  
         // this even works if chocolate is plural
-        System.out.println(imported);
+        System.out.println(taxType);
     }
     
 } 
-    
- 
+
+
+// System.out.println(arlist.get(3).indexOf("chocolate") != -1 ? true : false ); // check to see if a string has a word  
